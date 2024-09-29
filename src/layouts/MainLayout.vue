f5deb69e31681b973133454cee7bee6f03e59f1f
autoclickvn <autoclickvn@gmail.com>
2024-09-30 01:28:22 +0700

    Update UI sidebar

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MenuLayout from './menu/MenuLayout.vue';
+</script>
+
+<template>
+  <div >
+    <el-container>
+      <el-aside width="280px">
+        <MenuLayout/>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
